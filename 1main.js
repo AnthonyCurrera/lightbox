3b5d8d3d749f63eac5d0f48b21cab1993314fda5
Anthony Currera <31192158+AnthonyCurrera@users.noreply.github.com>
2018-04-29 19:08:48 -0500

    Add files via upload

---
+++ 1main.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+	$(".wrapper a").lightBox();
+});
+
+
+$("#search").keypress(function(){
+  var current_query = $("#search").val();
+
+  $(".image a").hide();
+
+  $(".image a").each(function(){
+
+    var current_keyword = $(this).attr("data-title");
+
+    if (current_keyword.indexOf(current_query) >= 0){
+
+      $(this).show();
+    }
+  });
+});
